39d8ef3685e71b234c1d86b6749e59075cc9ddda
celebrater <tkgm45nbchk@gmail.com>
2023-03-04 15:25:06 +0900

    first commit

---
+++ duplicater.ps1
@@ -0,0 +1,12 @@
+﻿$indir = 'J:\Dropbox\PC5_cloud\pg\PowerShell\duplicater\input'
+$outdir = 'J:\Dropbox\PC5_cloud\pg\PowerShell\duplicater\output'
+
+if (-not (Test-Path $outdir)){
+    New-Item $outdir -ItemType Directory
+}
+
+Get-ChildItem -Path $indir/*.txt | foreach{
+    $infile = $indir + "\" + $_.Name
+    $outfile = $outdir + "\copied-" + $_.Name
+    Copy-Item $infile -Destination $outfile
+}
